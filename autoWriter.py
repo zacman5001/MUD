17a10c167c9f10b44f5df261eeaba4c3e0dd1cb5
zacman5001 <isaacwmorrison@gmail.com>
2016-03-14 18:29:41 -0400

    Clean up #2

--- autoWriter.py
+++
@@ -1,6 +0,0 @@
-index = 1
-for x in range (10, 14):
-  for y in range (1, 18):
-    print "  <string name=\"lin1\" index=\"" + str(index) + "\">" + str(x) + "00" + str(-y) + "</string>"
-    index = index + 1
-m = raw_input
